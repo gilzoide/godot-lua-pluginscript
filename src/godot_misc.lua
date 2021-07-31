a34928a2ac1b0db964c99c7506316e8ba8f02abd
gilzoide <gilzoide@gmail.com>
2021-07-31 19:46:22 -0300

    Add Object.{p,}call and method bindings

--- src/godot_misc.lua
+++ src/godot_misc.lua
@@ -43,11 +43,39 @@ RID = ffi.metatype('godot_rid', {
 	__lt = api.godot_rid_operator_less,
 })
 
+ffi.cdef[[typedef struct lps_method_bind { godot_string method; } lps_method_bind]]
+local MethodBind = ffi.metatype('lps_method_bind', {
+	__new = function(mt, method)
+		return ffi.new(mt, String(method))
+	end,
+	__call = function(self, var, ...)
+		return var:call(self.method, ...)
+	end,
+})
+
+local object_methods = {
+	tovariant = ffi.C.hgdn_new_object_variant,
+	varianttype = GD.TYPE_OBJECT,
+	pcall = function(self, ...)
+		return Variant(self):pcall(...)
+	end,
+	call = function(self, ...)
+		return Variant(self):call(...)
+	end,
+}
 Object = ffi.metatype('godot_object', {
 	__tostring = GD.tostring,
-	__index = {
-		tovariant = ffi.C.hgdn_new_object_variant,
-		varianttype = GD.TYPE_OBJECT,
-	},
+	__index = function(self, key)
+		local method = object_methods[key]
+		if method then
+			return method
+		end
+		local var = object_methods.tovariant(self)
+		if self:call('has_method', key) then
+			return MethodBind(key)
+		else
+			self:call('get', key)
+		end
+	end,
 	__concat = concat_gdvalues,
 })
