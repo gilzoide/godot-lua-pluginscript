87d7bdef32918d48560bd9f2232060efb90aff35
gilzoide <gilzoide@gmail.com>
2021-07-31 08:28:15 -0300

    Move NodePath definition to misc file

--- src/godot_misc.lua
+++ src/godot_misc.lua
@@ -1,3 +1,26 @@
+local node_path_methods = {
+	tovariant = ffi.C.hgdn_new_node_path_variant,
+	varianttype = GD.TYPE_NODE_PATH,
+}
+NodePath = ffi.metatype('godot_node_path', {
+	__new = function(mt, text_or_nodepath)
+		local self = ffi.new(mt)
+		if ffi.istype(mt, text_or_nodepath) then
+			api.godot_node_path_new_copy(self, text_or_nodepath)
+		elseif ffi.istype(String, text_or_nodepath) then
+			api.godot_node_path_new(self, text_or_nodepath)
+		else
+			api.godot_node_path_new(self, String(text_or_nodepath))
+		end
+		return self
+	end,
+	__gc = api.godot_node_path_destroy,
+	__tostring = function(self)
+		return tostring(api.godot_node_path_as_string(self))
+	end,
+	__index = node_path_methods,
+})
+
 RID = ffi.metatype('godot_rid', {
 	__tostring = GD.tostring,
 	__index = {
--- src/godot_string.lua
+++ src/godot_string.lua
@@ -82,26 +82,3 @@ StringName = ffi.metatype('godot_string_name', {
 	end,
 	__index = string_name_methods,
 })
-
-local node_path_methods = {
-	tovariant = ffi.C.hgdn_new_node_path_variant,
-	varianttype = GD.TYPE_NODE_PATH,
-}
-NodePath = ffi.metatype('godot_node_path', {
-	__new = function(mt, text_or_nodepath)
-		local self = ffi.new(mt)
-		if ffi.istype(mt, text_or_nodepath) then
-			api.godot_node_path_new_copy(self, text_or_nodepath)
-		elseif ffi.istype(String, text_or_nodepath) then
-			api.godot_node_path_new(self, text_or_nodepath)
-		else
-			api.godot_node_path_new(self, String(text_or_nodepath))
-		end
-		return self
-	end,
-	__gc = api.godot_node_path_destroy,
-	__tostring = function(self)
-		return tostring(api.godot_node_path_as_string(self))
-	end,
-	__index = node_path_methods,
-})
