37ae2328d33bb383f87afff46fbbd731f16c58ae
gilzoide <gilzoide@gmail.com>
2021-08-04 12:59:26 -0300

    Remove clutter and add only code on par with the blog

--- src/language_gdnative.c
+++ src/language_gdnative.c
@@ -1,163 +1,123 @@
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+// HGDN already includes godot-headers
+#include "hgdn.h"
 
-#include "language_gdnative.h"
-
-// Callbacks definition
-void (*lps_language_add_global_constant_cb)(const godot_string *name, const godot_variant *value);
-godot_error (*lps_script_init_cb)(godot_pluginscript_script_manifest *data, const godot_string *path, const godot_string *source);
-void (*lps_script_finish_cb)(godot_pluginscript_script_data *data);
-godot_pluginscript_instance_data *(*lps_instance_init_cb)(godot_pluginscript_script_data *data, godot_object *owner);
-void (*lps_instance_finish_cb)(godot_pluginscript_instance_data *data);
-godot_bool (*lps_instance_set_prop_cb)(godot_pluginscript_instance_data *data, const godot_string *name, const godot_variant *value);
-godot_bool (*lps_instance_get_prop_cb)(godot_pluginscript_instance_data *data, const godot_string *name, godot_variant *ret);
-void (*lps_instance_call_method_cb)(godot_pluginscript_instance_data *data, const godot_string_name *method, const godot_variant **args, int argcount, godot_variant *ret, godot_variant_call_error *error);
-void (*lps_instance_notification_cb)(godot_pluginscript_instance_data *data, int notification);
-
-// Language functions
-static void *lps_alloc(void *userdata, void *ptr, size_t osize, size_t nsize) {
-	if (nsize == 0) {
-		hgdn_free(ptr);
-		return NULL;
-	}
-	else {
-		return hgdn_realloc(ptr, nsize);
-	}
+// Called when our language runtime will be initialized
+godot_pluginscript_language_data *lps_language_init() {
+    // TODO
+    return NULL;
 }
 
-static int lps_lua_touserdata(lua_State *L) {
-	const void *ptr = lua_topointer(L, 1);
-	lua_pushlightuserdata(L, (void *) ptr);
-	return 1;
+// Called when our language runtime will be terminated
+void lps_language_finish(godot_pluginscript_language_data *data) {
+    // TODO
 }
 
-static godot_pluginscript_language_data *lps_language_init() {
-	lua_State *L = lua_newstate(&lps_alloc, NULL);
-	lua_register(L, "touserdata", &lps_lua_touserdata);
-	luaL_openlibs(L);
-	if (luaL_dostring(L, LUA_INIT_SCRIPT) != 0) {
-		const char *error_msg = lua_tostring(L, -1);
-		HGDN_PRINT_ERROR("Error running initialization script: %s", error_msg);
-	}
-	return L;
+// Called when Godot registers globals in the language, such as Autoload nodes
+void lps_language_add_global_constant(godot_pluginscript_language_data *data, const godot_string *name, const godot_variant *value) {
+    // TODO
 }
 
-static void lps_language_finish(godot_pluginscript_language_data *data) {
-	lua_State *L = (lua_State *) data;
-	lua_gc(L, LUA_GCCOLLECT, 0);
-	lua_close(L);
+// Called when a Lua script is loaded, e.g.: const SomeScript = preload("res://some_script.lua")
+godot_pluginscript_script_manifest lps_script_init(godot_pluginscript_language_data *data, const godot_string *path, const godot_string *source, godot_error *error) {
+    godot_pluginscript_script_manifest manifest = {};
+    // All Godot objects must be initialized, or else our plugin SEGFAULTs
+    hgdn_core_api->godot_string_name_new_data(&manifest.name, "");
+    hgdn_core_api->godot_string_name_new_data(&manifest.base, "");
+    hgdn_core_api->godot_dictionary_new(&manifest.member_lines);
+    hgdn_core_api->godot_array_new(&manifest.methods);
+    hgdn_core_api->godot_array_new(&manifest.signals);
+    hgdn_core_api->godot_array_new(&manifest.properties);
+    // TODO
+    return manifest;
 }
 
-static void lps_language_add_global_constant(godot_pluginscript_language_data *data, const godot_string *name, const godot_variant *value) {
-	lps_language_add_global_constant_cb(name, value);
-}
-
-// Script manifest
-static godot_pluginscript_script_manifest lps_script_init(godot_pluginscript_language_data *data, const godot_string *path, const godot_string *source, godot_error *error) {
-	godot_pluginscript_script_manifest manifest = {};
-	manifest.data = data;
-	hgdn_core_api->godot_string_name_new_data(&manifest.name, "");
-	hgdn_core_api->godot_string_name_new_data(&manifest.base, "");
-	hgdn_core_api->godot_dictionary_new(&manifest.member_lines);
-	hgdn_core_api->godot_array_new(&manifest.methods);
-	hgdn_core_api->godot_array_new(&manifest.signals);
-	hgdn_core_api->godot_array_new(&manifest.properties);
-
-	godot_error ret = lps_script_init_cb(&manifest, path, source);
-	if (error) {
-		*error = ret;
-	}
-
-	return manifest;
-}
-
-static void lps_script_finish(godot_pluginscript_script_data *data) {
-	lps_script_finish_cb(data);
+// Called when a Lua script is unloaded
+void lps_script_finish(godot_pluginscript_script_data *data) {
+    // TODO
 }
 
 
-// Instance
-static godot_pluginscript_instance_data *lps_instance_init(godot_pluginscript_script_data *data, godot_object *owner) {
-	return lps_instance_init_cb(data, owner);
+// Called when a Script Instance is being created, e.g.: var instance = SomeScript.new()
+godot_pluginscript_instance_data *lps_instance_init(godot_pluginscript_script_data *data, godot_object *owner) {
+    // TODO
+    return NULL;
 }
 
-static void lps_instance_finish(godot_pluginscript_instance_data *data) {
-	lps_instance_finish_cb(data);
+// Called when a Script Instance is being finalized
+void lps_instance_finish(godot_pluginscript_instance_data *data) {
+    // TODO
 }
 
-static godot_bool lps_instance_set_prop(godot_pluginscript_instance_data *data, const godot_string *name, const godot_variant *value) {
-	return lps_instance_set_prop_cb(data, name, value);
+// Called when setting a property on an instance, e.g.: instance.prop = value
+godot_bool lps_instance_set_prop(godot_pluginscript_instance_data *data, const godot_string *name, const godot_variant *value) {
+    // TODO
+    return false;
 }
 
-static godot_bool lps_instance_get_prop(godot_pluginscript_instance_data *data, const godot_string *name, godot_variant *ret) {
-	return lps_instance_get_prop_cb(data, name, ret);
+// Called when getting a property on an instance, e.g.: var value = instance.prop
+godot_bool lps_instance_get_prop(godot_pluginscript_instance_data *data, const godot_string *name, godot_variant *ret) {
+    // TODO
+    return false;
 }
 
-static godot_variant lps_instance_call_method(godot_pluginscript_instance_data *data, const godot_string_name *method, const godot_variant **args, int argcount, godot_variant_call_error *error) {
-	godot_variant var = hgdn_new_nil_variant();
-	lps_instance_call_method_cb(data, method, args, argcount, &var, error);
-	return var;
+// Called when calling a method on an instance, e.g.: instance.method(args)
+godot_variant lps_instance_call_method(godot_pluginscript_instance_data *data, const godot_string_name *method, const godot_variant **args, int argcount, godot_variant_call_error *error) {
+    // TODO
+    return hgdn_new_nil_variant();
 }
 
-static void lps_instance_notification(godot_pluginscript_instance_data *data, int notification) {
-	lps_instance_notification_cb(data, notification);
+// Called when a notification is sent to instance
+void lps_instance_notification(godot_pluginscript_instance_data *data, int notification) {
+    // TODO
 }
 
-static godot_pluginscript_language_desc lps_language_desc = {
-	.name = "Lua",
-	.type = "Lua",
-	.extension = "lua",
-	.recognized_extensions = (const char *[]){ "lua", NULL },
-	.init = &lps_language_init,
-	.finish = &lps_language_finish,
-	.reserved_words = (const char *[]){
-		// Lua keywords
-		"and", "break", "do", "else", "elseif", "end",
-		"false", "for", "function", "goto", "if", "in",
-		"local", "nil", "not", "or", "repeat", "return",
-		"then", "true", "until", "while",
-		// Other remarkable identifiers
-		"self", "_G", "_VERSION",
-#if LUA_VERSION_NUM >= 502
-		"_ENV",
-#endif
-		"bool", "int", "float",
-		NULL
-	},
-	.comment_delimiters = (const char *[]){ "--", "--[[ ]]", NULL },
-	.string_delimiters = (const char *[]){ "' '", "\" \"", "[[ ]]", "[=[ ]=]", NULL },
-	.has_named_classes = false,
-	.supports_builtin_mode = false,
-	.add_global_constant = &lps_language_add_global_constant,
+// Declared as a global variable, because Godot needs the
+// memory to be valid until our plugin is unloaded 
+godot_pluginscript_language_desc lps_language_desc = {
+    .name = "Lua",
+    .type = "Lua",
+    .extension = "lua",
+    .recognized_extensions = (const char *[]){ "lua", NULL },
+    .reserved_words = (const char *[]){
+        // Lua keywords
+        "and", "break", "do", "else", "elseif", "end",
+        "false", "for", "function", "goto", "if", "in",
+        "local", "nil", "not", "or", "repeat", "return",
+        "then", "true", "until", "while",
+        // Other important identifiers
+        "self", "_G", "_ENV", "_VERSION",
+        NULL
+    },
+    .comment_delimiters = (const char *[]){ "--", "--[[ ]]", NULL },
+    .string_delimiters = (const char *[]){ "' '", "\" \"", "[[ ]]", "[=[ ]=]", NULL },
+    // Lua scripts don't care about the class name
+    .has_named_classes = false,
+    // Builtin scripts didn't work for me, so disable for now...
+    .supports_builtin_mode = false,
 
-	.script_desc = {
-		.init = &lps_script_init,
-		.finish = &lps_script_finish,
-		.instance_desc = {
-			.init = &lps_instance_init,
-			.finish = &lps_instance_finish,
-			.set_prop = &lps_instance_set_prop,
-			.get_prop = &lps_instance_get_prop,
-			.call_method = &lps_instance_call_method,
-			.notification = &lps_instance_notification,
-		},
-	},
+    // Callbacks
+    .init = &lps_language_init,
+    .finish = &lps_language_finish,
+    .add_global_constant = &lps_language_add_global_constant,
+    .script_desc = {
+        .init = &lps_script_init,
+        .finish = &lps_script_finish,
+        .instance_desc = {
+            .init = &lps_instance_init,
+            .finish = &lps_instance_finish,
+            .set_prop = &lps_instance_set_prop,
+            .get_prop = &lps_instance_get_prop,
+            .call_method = &lps_instance_call_method,
+            .notification = &lps_instance_notification,
+        },
+    },
 };
 
-// GDNative functions
+// GDN_EXPORT makes sure our symbols are exported in the way Godot expects
+// This is not needed, since symbols are exported by default, but it
+// doesn't hurt being explicit about it
 GDN_EXPORT void godot_gdnative_init(godot_gdnative_init_options *options) {
 	hgdn_gdnative_init(options);
-
-	if (!hgdn_pluginscript_api) {
-		HGDN_PRINT_ERROR("PluginScript is not supported!");
-		return;
-	}
-
-	if (options->in_editor) {
-		lps_register_in_editor_callbacks(&lps_language_desc);
-	}
-
 	hgdn_pluginscript_api->godot_pluginscript_register_language(&lps_language_desc);
 }
 
@@ -167,3 +127,4 @@ GDN_EXPORT void godot_gdnative_terminate(godot_gdnative_terminate_options *optio
 
 GDN_EXPORT void godot_gdnative_singleton() {
 }
+
--- xmake.lua
+++ xmake.lua
@@ -1,66 +1,12 @@
-if GD then return {} end
-
-option("use-luajit")
-	set_description("Use LuaJIT instead of vanilla Lua")
-	set_default(true)
-	set_showmenu(true)
-option("system-lua")
-	set_description("Use Lua/LuaJIT system package, if available")
-	set_default(false)
-	set_showmenu(true)
-option_end()
-
-local lua_or_jit = has_config("use-luajit") and "luajit" or "lua"
-add_requires(lua_or_jit, {
-	system = has_config("system-lua") or false,
-	config = {
-		gc64 = true,
-	},
-})
-
--- Embed any file into a `#include`able .h file
-rule("embed_header")
-	on_build_file(function(target, sourcefile, opt)
-		local target_file = path.join(get_config('buildir'), "include", path.filename(sourcefile) .. ".h")
-		if os.isfile(target_file) and os.mtime(target_file) > os.mtime(sourcefile) then
-			return
-		end
-
-		cprint("${bright green}[%3d%%]:${clear} embed_header %s", opt.progress, sourcefile)
-		local header_contents = {}
-		for line in io.lines(sourcefile) do
-			local escaped_line = line:gsub('\\', '\\\\'):gsub('"', '\\"')
-			table.insert(header_contents, '"' .. escaped_line .. '\\n"')
-		end
-		header_contents = table.concat(header_contents, '\n')
-		io.writefile(target_file, header_contents)
-	end)
-rule_end()
-
-target("embed_lua_files")
-	set_kind("object")
-	add_files("src/*.lua", { rule = "embed_header" })
-target("cat_init_script")
-	set_kind("phony")
-	set_default(false)
-	on_build(function(target)
-		local buildir = get_config('buildir')
-		local embedded_files = {}
-		for line in io.lines("src/lua_init_script.c") do
-			local m = line:match("#include%W*([^%.]+%.lua%.h)")
-			if m then
-				table.insert(embedded_files, io.readfile(path.join(buildir, "include", m)))
-			end
-		end
-		io.writefile(path.join(buildir, "include", "init_script.lua"), table.concat(embedded_files, '\n'))
-	end)
+-- xmake.lua
 target("lua_pluginscript")
 	set_kind("shared")
+	-- Set the output name to something easy to find like `build/lua_pluginscript_linux_x86_64.so`
 	set_targetdir("$(buildir)")
 	set_prefixname("")
 	set_suffixname("_$(os)_$(arch)")
+	-- Add "-I" flags for locating HGDN and godot-header files
+	add_includedirs("lib/godot-headers", "lib/high-level-gdnative")
+	-- src/hgdn.c, src/language_gdnative.c
 	add_files("src/*.c")
-	add_deps("embed_lua_files")
-	add_includedirs("lib/godot-headers", "lib/high-level-gdnative", "$(buildir)/include")
-	add_packages(lua_or_jit)
 target_end()
