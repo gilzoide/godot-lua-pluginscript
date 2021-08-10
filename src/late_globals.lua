5cd46bef02c4db84e6c3268dabd541852808cd50
gilzoide <gilzoide@gmail.com>
2021-08-10 12:28:21 -0300

    Add searcher for Lua modules using File, add/patch package.searchpath, add res:// to Lua path

--- src/late_globals.lua
+++ src/late_globals.lua
@@ -29,3 +29,49 @@ Thread = Class:new("_Thread")
 Mutex = Class:new("_Mutex")
 Semaphore = Class:new("_Semaphore")
 
+local dirsep, pathsep, path_mark = package.config:match("^([^\n]*)\n([^\n]*)\n([^\n]+)")
+local template_pattern = '[^' .. pathsep .. ']+'
+local function searchpath(name, path, sep, rep)
+	sep = sep or '.'
+	rep = rep or '/'
+	if sep ~= '' then
+		name = name:gsub(sep:gsub('.', '%%%0'), rep)
+	end
+	local notfound = {}
+	local f = File:new()
+	for template in path:gmatch(template_pattern) do
+		local filename = template:gsub(path_mark, name)
+		if f:open(filename, File.READ) == GD.OK then
+			return filename, f
+		else
+			table.insert(notfound, string.format("\n\tno file %q", filename))
+		end
+	end
+	return nil, table.concat(notfound)
+end
+
+local function lua_searcher(name)
+	local filename, open_file_or_err = searchpath(name, package.path)
+	if not filename then
+		return nil, open_file_or_err
+	end
+	local file_len = open_file_or_err:get_len()
+	local contents = open_file_or_err:get_buffer(file_len):get_string()
+	open_file_or_err:close()
+	return assert(loadstring(contents, filename))
+end
+
+function package.searchpath(...)
+	local filename, open_file_or_err = searchpath(...)
+	if not filename then
+		return nil, open_file_or_err
+	else
+		open_file_or_err:close()
+		return filename
+	end
+end
+
+local searchers = package.searchers or package.loaders
+searchers[2] = lua_searcher
+
+package.path = 'res://?.lua' .. pathsep .. 'res://?/init.lua' .. pathsep .. package.path
