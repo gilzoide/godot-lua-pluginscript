dca474c7415c81c5591186ea7baaa8f6e512e563
gilzoide <gilzoide@gmail.com>
2021-08-11 10:31:31 -0300

    Add MIT (Expat) license

--- src/language_gdnative.c
+++ src/language_gdnative.c
@@ -1,3 +1,27 @@
+/**
+ * @file language_gdnative.c  GDNative + PluginScript callbacks
+ * This file is part of Godot Lua PluginScript: https://github.com/gilzoide/godot-lua-pluginscript
+ *
+ * Copyright (C) 2021 Gil Barbosa Reis.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 #include "lua.h"
 #include "lualib.h"
 #include "lauxlib.h"
@@ -183,6 +207,7 @@ GDN_EXPORT void godot_gdnative_init(godot_gdnative_init_options *options) {
 	else {
 		lps_active_library_path = hgdn_string_get(options->active_library_path);
 	}
+		hgdn_print("LIB %s", lps_active_library_path.ptr);
 
 	hgdn_pluginscript_api->godot_pluginscript_register_language(&lps_language_desc);
 }
--- src/lps_class_metadata.lua
+++ src/lps_class_metadata.lua
@@ -1,3 +1,26 @@
+-- @file lps_class_metadata.lua  Helpers for Script/Class metadata (properties, methods, signals)
+-- This file is part of Godot Lua PluginScript: https://github.com/gilzoide/godot-lua-pluginscript
+--
+-- Copyright (C) 2021 Gil Barbosa Reis.
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the “Software”), to
+-- deal in the Software without restriction, including without limitation the
+-- rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+-- sell copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+-- IN THE SOFTWARE.
+
 -- Map names and ctypes to godot_variant_type
 local property_types = {
 	bool = GD.TYPE_BOOL, [bool] = GD.TYPE_BOOL,
