ea05aa14187dac5eb73badedbb180214adf68528
gilzoide <gilzoide@gmail.com>
2021-08-31 11:28:11 -0300

    Move repl files to plugin folder, add plugin to dist

--- plugin/plugin.gd
+++ plugin/plugin.gd
@@ -4,12 +4,12 @@ extends EditorPlugin
 var _lua_repl
 
 func _enter_tree() -> void:
-	_lua_repl = preload("repl/lua_repl.tscn").instance()
+	_lua_repl = preload("lua_repl.tscn").instance()
 	var repl_button = add_control_to_bottom_panel(_lua_repl, "Lua REPL")
 
 
 func _exit_tree() -> void:
 	if _lua_repl:
 		remove_control_from_bottom_panel(_lua_repl)
-		_lua_repl.queue_free()
+		_lua_repl.free()
 		_lua_repl = null
