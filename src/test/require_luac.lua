3bcd7608d1308d2fa279c5e9239ac2c3a3f2d8e1
gilzoide <gilzoide@gmail.com>
2023-02-21 10:35:26 -0300

    Fix rename luac -> cmodule

--- src/test/require_luac.lua
+++ src/test/require_luac.lua
@@ -3,7 +3,7 @@ local lu = require "luaunit"
 local Test = {}
 
 function Test:test_require_luac_module()
-	lu.assert_true(pcall(require, 'test_luac'))
+	lu.assert_true(pcall(require, 'test_cmodule'))
 end
 
 return Test
